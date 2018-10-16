56cec8505949b4f0febfd9d54f10450de82731ac
gleysonabreu <gleyson_datu@hotmail.com>
2018-10-16 15:20:16 -0300

    Criamos a classe Usuário

---
+++ class/Usuario.php
@@ -0,0 +1,62 @@
+<?php
+	/**
+	 * Usuario
+	 */
+	class Usuario
+	{
+		private $id;
+		private $login;
+		private $senha;
+		private $dtc;
+		
+		public function getId():int{
+			return $this->id;
+		}
+		public function setId($id){
+			$this->id = $id;
+		}
+		public function getLogin(){
+			return $this->login;
+		}
+		public function setLogin($lg){
+			$this->login = $lg;
+		}
+		public function getSenha(){
+			return $this->senha;
+		}
+		public function setSenha($pass){
+			$this->senha = $pass;
+		}
+		public function getDtc(){
+			return $this->dtc;
+		}
+		public function setDtc($dtc){
+			$this->dtc = $dtc;
+		}
+
+		public function loadById($id){
+			$sql = new Sql();
+			$result = $sql->select("SELECT * FROM tbl_usuarios WHERE id = :ID", array(
+				":ID" => $id
+			));
+
+			if(count($result) > 0 ){
+				$row = $result[0];
+
+				$this->setId($row['id']);
+				$this->setLogin($row['login']);
+				$this->setSenha($row['senha']);
+				$this->setDtc(new DateTime($row['dtc']));
+			}
+		}
+
+		public function __toString(){
+			return json_encode(array(
+				"id"=>$this->getId(),
+				"login"=>$this->getLogin(),
+				"senha"=>$this->getSenha(),
+				"dtc"=>$this->getDtc()->format("d/m/Y H:i:s")
+			));
+		}
+	}
+?>
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 	require_once "config.php";
 
-	$sql = new Sql();
+	// $sql = new Sql();
 
-	$usuarios = $sql->select("SELECT * FROM tbl_usuarios");
+	// $usuarios = $sql->select("SELECT * FROM tbl_usuarios");
 
-	echo json_encode($usuarios);
+	// echo json_encode($usuarios);
+
+	$root = new Usuario();
+
+	$root->loadById(1);
+
+	echo $root;
 ?>
